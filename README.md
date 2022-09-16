0d935edb43f7d28e589a70d5005f96844ab671ce
kaheilaw0106 <kahei0106@outlook.com>
2022-09-16 14:21:01 +0800

    error handling & readme update

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Endpoints:
 ```
 current_time:  GET - https://yr3ejhev7i.execute-api.us-east-1.amazonaws.com/prod/current_time
 circulating_supply:  GET - https://yr3ejhev7i.execute-api.us-east-1.amazonaws.com/prod/circulating_supply
-cmc_price: GET - https://elzgxr8iga.execute-api.us-east-1.amazonaws.com/dev/cmc_price
+cmc_price: GET - https://wi51x7bbc4.execute-api.us-east-1.amazonaws.com/dev/cmc_price
 ```
 
 ## Local Testing
@@ -36,9 +36,7 @@ Set up AWS CLI and configure your AWS credentials.
 # cd to repo root
 sls deploy
 # or
-sls deploy --aws-profile [your-aws-cli-profile]
-# deploy to different stage
-sls deploy --stage [prod/dev]
+sls deploy --aws-profile [your-aws-cli-profile] --stage [prod/dev]
 ```
 
 After deployment, remember to enable CORS in API Gateway settings. See https://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-cors-console.html and https://www.serverless.com/blog/cors-api-gateway-survival-guide/
--- cmc_price/handler.js
+++ cmc_price/handler.js
@@ -2,36 +2,39 @@ const fetch = require("node-fetch");
 const COIN_MARKET_CAP_API =
   "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest";
 
-module.exports.cmcPrice = async (event, callback) => {
+module.exports.cmcPrice = async (event) => {
   let id = "";
   let responseCode = 200;
+  let finalResult = "";
 
   if (event.queryStringParameters && event.queryStringParameters.id) {
     id = event.queryStringParameters.id;
   }
 
-  const cmcData = await fetch(`${COIN_MARKET_CAP_API}?id=${id}`, {
-    method: "GET",
-    headers: {
-      "X-CMC_PRO_API_KEY": "714bbf0f-a0ce-4bd3-85d2-5b291eef395c",
-      "Access-Control-Allow-Origin": "*",
-      "content-type": "application/json",
-    },
-  });
-
-  const result = await cmcData.json();
-
-  const finalResult = Object.values(result.data).reduce((res, item) => {
-    return {
-      ...res,
-      [item.id]: {
-        percent_change_24h: item["quote"]["USD"]["percent_change_24h"],
-        price: item["quote"]["USD"]["price"],
+  try {
+    const cmcData = await fetch(`${COIN_MARKET_CAP_API}?id=${id}`, {
+      method: "GET",
+      headers: {
+        "X-CMC_PRO_API_KEY": "714bbf0f-a0ce-4bd3-85d2-5b291eef395c",
+        "Access-Control-Allow-Origin": "*",
+        "content-type": "application/json",
       },
-    };
-  }, {});
+    });
+    const result = await cmcData.json();
+    finalResult = Object.values(result.data).reduce((res, item) => {
+      return {
+        ...res,
+        [item.id]: {
+          percent_change_24h: item["quote"]["USD"]["percent_change_24h"],
+          price: item["quote"]["USD"]["price"],
+        },
+      };
+    }, {});
+  } catch (error) {
+    responseCode = 400;
+  }
 
-  let response = {
+  const response = {
     statusCode: responseCode,
     headers: {
       "content-type": "application/json",
@@ -40,5 +43,5 @@ module.exports.cmcPrice = async (event, callback) => {
     body: JSON.stringify(finalResult),
   };
 
-  callback(null, response);
+  return response;
 };
