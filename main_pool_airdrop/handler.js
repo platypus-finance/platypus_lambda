ac9d1e3563ba3d310e28e049ef40e358e413f0c3
ChrisChan888 <15077693d@gmail.com>
2023-02-24 12:28:23 +0800

    add mock user csv

--- main_pool_airdrop/handler.js
+++ main_pool_airdrop/handler.js
@@ -1,29 +1,31 @@
 const { ethers } = require("ethers");
+const path = require("path");
+let csvToJson = require("convert-csv-to-json");
 module.exports.mainPoolAirdrop = async (event) => {
   const message = "Please sign to read your airdrop.";
   const wallet = ethers.Wallet.createRandom();
   let signature = await wallet.signMessage(message);
+  let users = csvToJson
+    .fieldDelimiter(",")
+    .getJsonFromCsv(path.join(__dirname, "users.csv"));
   const responseCode = 200;
   if (event.queryStringParameters && event.queryStringParameters.signature) {
     signature = event.queryStringParameters.signature;
   }
-  const finalResult = {
-    address: ethers.verifyMessage(message, signature),
-    uspBalance: 0,
-    usdteLpBalance: 0,
-    usdceLpBalance: 0,
-    daieLpBalance: 0,
-    usdcLpBalance: 0,
-    usdtLpBalance: 0,
-    busdLpBalance: 0,
-  };
+  const address = ethers.verifyMessage(message, signature);
+  let user = null;
+  if (address) {
+    user = users.filter(
+      (data) => data["address"].toLowerCase() === address.toLowerCase()
+    )[0];
+  }
   const response = {
     statusCode: responseCode,
     headers: {
       "content-type": "application/json",
       "Access-Control-Allow-Origin": "*",
     },
-    body: JSON.stringify(finalResult),
+    body: JSON.stringify(user),
   };
 
   return response;
