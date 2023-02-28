f2fe61ec78aef9f6fd21fcb39e0510743ff27f14
shinji2 <shinji@platypus.finance>
2023-02-28 11:08:05 +0800

    catch and respond with error

--- main_pool_airdrop/handler.js
+++ main_pool_airdrop/handler.js
@@ -3,34 +3,47 @@ const path = require("path");
 var fs = require("fs");
 
 module.exports.mainPoolAirdrop = async (event) => {
-  const message =
+  const MESSSAGE =
     "Please sign this message to access the info of your compensation regarding the USP hack.";
-  const wallet = ethers.Wallet.createRandom();
-  let signature = await wallet.signMessage(message);
-  var data = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "users.json"), "utf8")
-  );
-  const responseCode = 200;
-  if (event.queryStringParameters && event.queryStringParameters.signature) {
-    signature = event.queryStringParameters.signature;
-  }
-  const address = ethers.verifyMessage(message, signature);
-  let user = null;
-  if (address) {
-    user = data["users"][address.toLowerCase()];
-  }
-  const result = {
-    market: data["market"],
-    ...user,
-  };
-  const response = {
-    statusCode: responseCode,
-    headers: {
-      "content-type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-    body: JSON.stringify(result),
-  };
 
-  return response;
+  try {
+    var data = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "users.json"), "utf8")
+    );
+    const responseCode = 200;
+    let signature = "";
+    if (event.queryStringParameters && event.queryStringParameters.signature) {
+      signature = event.queryStringParameters.signature;
+    }
+    const address = ethers.verifyMessage(MESSSAGE, signature);
+    let user = null;
+    if (address) {
+      user = data["users"][address.toLowerCase()];
+    }
+    const result = {
+      market: data["market"],
+      ...user,
+    };
+    const response = {
+      statusCode: responseCode,
+      headers: {
+        "content-type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(result),
+    };
+
+    return response;
+  } catch (err) {
+    const response = {
+      statusCode: 500,
+      headers: {
+        "content-type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(err),
+    };
+
+    return response;
+  }
 };
